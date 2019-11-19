48c235e9e1223f79eab7b545480dc5741ad5e259
rodrigosoriaUTN <rodrigo.soria98@hotmail.com>
2019-11-19 12:53:26 -0300

    added product detail component and router redirector

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from '../components/product-list/product-list.component';
 import { SignInComponent } from '../components/sign-in/sign-in.component';
+import { ProductDetailComponent } from '../components/product-detail/product-detail.component';
 
 const appRoutes : Routes = [
   {path: 'products', component: ProductListComponent},
+  {path: 'products/:id', component: ProductDetailComponent}
   {path: '', redirectTo: 'products', pathMatch: 'full'},
   {path: 'signing', component: SignInComponent}
 ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductRowComponent } from './components/product-row/product-row.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     SignInComponent,
     ProductListComponent,
     ProductRowComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule, 
